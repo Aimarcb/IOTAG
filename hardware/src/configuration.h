82d02359c91c82bd8703af852aebfb37344003bd
Gael Cana <gaelcanablanco03@gmail.com>
2026-07-08 00:27:04 +0200

    hardware añadido

---
+++ hardware/src/configuration.h
@@ -0,0 +1,13 @@
+
+#ifndef CONFIGURATION_H
+#define CONFIGURATION_H
+
+const char* WIFI_SSID = "DIGIFIBRA-xx3D";
+const char* WIFI_PASSW = "kXQ2Nkfy4Yxf";
+
+const char* MQTT_SERVER = "";
+const int MQTT_PORT = 1883;
+
+#endif
+
+
