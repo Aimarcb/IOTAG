40fee0de4fd4f3ecf74fbf0e97b3e46f133a1f7e
Gael Cana <gaelcanablanco03@gmail.com>
2026-07-08 15:23:48 +0200

    Implemetacion de la IA con Gemini terminado

---
+++ backend/ia/prompt.py
@@ -0,0 +1,26 @@
+
+
+def generar_prompt(temperatura,luz,movimiento, orden_usuario):
+    """
+    Genera el texto maestro con las reglas para Gemini inyectando los datos reales.
+    """
+    texto_presencia = "Sí" if movimiento == 1 else "No"
+    
+    return f"""
+    Eres el sistema domótico inteligente de mi habitación.
+    Acabas de recibir una orden por texto del usuario y estos son los datos actuales de los sensores en la habitación:
+
+    ESTADO REAL DE LA HABITACIÓN (Desde la Base de Datos):
+    - Temperatura actual: {temperatura}ºC
+    - Porcentaje de luz ambiental: {luz}%
+    - Movimiento detectado ahora mismo: {texto_presencia}
+
+    ORDEN RECIBIDA DEL USUARIO: "{orden_usuario}"
+
+    REGLA ABSOLUTA: Responde ÚNICAMENTE con un objeto JSON válido. No uses texto plano, ni introducciones, ni bloques de código markdown (```json).
+    Estructura exacta del JSON:
+    {{
+      "respuesta_altavoz": "Una frase corta, inteligente y natural que explique qué has hecho basándote en los datos.",
+      "rele_luz": "ON" o "OFF",
+    }}
+    """
--- backend/main.py
+++ backend/main.py
@@ -10,3 +10,19 @@ def startup_event():
 @app.get("/")
 def read_root():
     return {"message": "NetHome API activa"}
+"""""
+## Código que añadirá tu hermano en su API:
+from fastapi import APIRouter
+from ia.ia_manager import procesar_orden_inteligente # <-- Importa tu función
+
+router = APIRouter()
+
+@router.get("/ia/orden")
+def api_consultar_ia(texto: str):
+    # Llama a tu cerebro pasándole la frase que llega del móvil
+    decision = procesar_orden_inteligente(texto)
+    
+    # Aquí tu hermano añadirá el código para mandar el MQTT al ESP32
+    # basado en lo que tú has decidido (decision['rele_luz'], etc.)
+    
+    return decision"""
